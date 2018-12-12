3139f92d02dc3b91ecd6c124df2a701aef3846ca
jj790d <jj790d@att.com>
2018-12-12 00:39:25 -0500

    Added Style for Header Component

--- neighborhood-map/src/App.js
+++ neighborhood-map/src/App.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Header from './components/Header';
 import './App.css';
 
+//Constants (In our case, the list of venueIDs used by FourSquare to get the data for our Places.
+const venueIDs = [
+    ''
+];
+
 class App extends Component {
-  render() {
-    return (
-      <div className="app">
-          <Header
-            title={"IN THE LOOP"}
-          />
-      </div>
-    );
-  }
+    state = {};
+
+    render() {
+        return (
+            <div className="app">
+                <Header
+                    title={"IN THE LOOP"}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
